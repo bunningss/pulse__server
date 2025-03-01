cc8f742e453c018c9d2aa470266e1373d3bcbff0
tayef mahmud <mahmudovi183@gmail.com>
2025-03-01 12:42:21 +0600

    save

--- socket/quiz.js
+++ socket/quiz.js
@@ -1,19 +1,23 @@
 import Game from "../models/Game.js";
 import Player from "../models/Player.js";
+import Question from "../models/Question.js";
 
 export default function QuizMode(io) {
-  const quiz = io.of("/quiz");
+  const quiz = io.of("socket/quiz");
 
   quiz.on("connection", (socket) => {
+    console.log(`user ${socket.id} connected`);
     // create game
-    socket.on("create-game", async ({ host }) => {
+    socket.on("create-game", async ({ host, questionId }) => {
       const code = Math.floor(100000 + Math.random() * 900000).toString();
+
+      const topic = await Question.findById(questionId).lean();
 
       const game = new Game({
         code,
         host,
         players: [],
-        questions: [],
+        questions: topic?.questions,
         isStarted: false,
         currentQuestionIndex: 0,
       });
@@ -21,8 +25,9 @@ export default function QuizMode(io) {
       try {
         await game.save();
         socket.join(code);
-        socket.emit("game-created", { code, gameId: game._id });
+        socket.emit("game-created", { code, message: "Game created." });
       } catch (error) {
+        console.log(error);
         socket.emit("error", { message: "Failed to create game" });
       }
     });
@@ -31,22 +36,29 @@ export default function QuizMode(io) {
     socket.on("join-game", async ({ gameCode, username }) => {
       try {
         const game = await Game.findOne({ code: gameCode });
+
         if (!game) return quiz.emit("error", { message: "Invalid game PIN." });
 
         const player = new Player({
           name: username,
           score: 0,
         });
-        await player.save();
 
         game.players.push(player._id);
         await game.save();
 
+        const updatedGame = await Game.findById(game._id)
+          .populate("players")
+          .lean();
+
         socket.join(gameCode);
-        quiz
-          .to(gameCode)
-          .emit("player-joined", { message: `${username} joined the game.` });
+        socket.emit("player-joined", {
+          message: `${username} joined the game.`,
+          code: gameCode,
+          players: updatedGame.players,
+        });
       } catch (error) {
+        console.log(error);
         socket.emit("join-error", { message: "Failed to join game." });
       }
     });
